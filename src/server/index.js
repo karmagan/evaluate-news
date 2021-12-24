906d39644bc5c6458b5fc82b5fcfe625407ffa21
Kadir Armagan <kadirarmagan@gmail.com>
2021-12-24 00:56:03 +0000

    css minify

--- src/server/index.js
+++ src/server/index.js
@@ -2,6 +2,8 @@ var path = require('path')
 const express = require('express')
 const mockAPIResponse = require('./mockAPI.js')
 
+const API_KEY = 'acc3a9854d3d44bac9227fd30b4b283f'
+
 const app = express()
 
 app.use(express.static('dist'))
@@ -14,10 +16,11 @@ app.get('/', function (req, res) {
 })
 
 // designates what port the app will listen to for incoming requests
-app.listen(8080, function () {
-    console.log('Example app listening on port 8080!')
+app.listen(3000, function () {
+    console.log('Example app listening on port 3000!')
 })
 
 app.get('/test', function (req, res) {
     res.send(mockAPIResponse)
 })
+
